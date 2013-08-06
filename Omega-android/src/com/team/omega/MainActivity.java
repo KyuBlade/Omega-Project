96708d3961182b6395755514be8509c9e01c4d9c
KyuBlade <jonesadev@gmail.com>
2013-08-06 17:49:09 +0200

    Share project Omega aandroid

---
+++ Omega-android/src/com/team/omega/MainActivity.java
@@ -0,0 +1,19 @@
+package com.team.omega;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.team.omega.core.GameCore;
+
+public class MainActivity extends AndroidApplication {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
+        cfg.useGL20 = false;
+        
+        initialize(new GameCore(), cfg);
+    }
+}
