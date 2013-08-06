0fcd2fcf534c50096c19e76a0953337b04b30413
KyuBlade <jonesadev@gmail.com>
2013-08-06 18:32:58 +0200

    Share project Omega-desktop

---
+++ Omega-desktop/src/com/team/omega/Config.java
@@ -0,0 +1,12 @@
+package com.team.omega;
+
+
+public class Config
+{
+    
+    public static String GAME_TITLE = "Omega Project";
+    public static int DISPLAY_WIDTH = 1280;
+    public static int DISPLAY_HEIGHT = 720;
+    public static boolean USE_GL20 = false;
+    
+}
---
+++ Omega-desktop/src/com/team/omega/Main.java
@@ -0,0 +1,23 @@
+package com.team.omega;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.team.omega.core.GameCore;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+	LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+	cfg.title = Config.GAME_TITLE;
+	cfg.useGL20 = Config.USE_GL20;
+	cfg.width = Config.DISPLAY_WIDTH;
+	cfg.height = Config.DISPLAY_HEIGHT;
+	cfg.vSyncEnabled = false;
+	cfg.foregroundFPS = 0;
+	
+	new LwjglApplication(new GameCore(), cfg);
+    }
+    
+}
