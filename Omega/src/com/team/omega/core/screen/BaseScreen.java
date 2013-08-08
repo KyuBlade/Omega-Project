4711d8686262ab04810be1e56258c6b92b9fca1c
KyuBlade <jonesadev@gmail.com>
2013-08-08 12:39:45 +0200

    Fix aspect ratio

--- Omega/src/com/team/omega/core/screen/BaseScreen.java
+++ Omega/src/com/team/omega/core/screen/BaseScreen.java
@@ -3,9 +3,11 @@ package com.team.omega.core.screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.Scaling;
 import com.team.omega.core.GameCore;
 
 
@@ -26,8 +28,6 @@ public class BaseScreen implements Screen
 	
 	// Debug purpose
 	layout.debug();
-	layout.debugTable();
-	layout.debugCell();
 	
 	// TODO: May do overload and be used once for all screen
 	// So make a SkinManager
@@ -50,7 +50,13 @@ public class BaseScreen implements Screen
     public void resize(int width, int height)
     {
 	Gdx.app.debug("Resizing", "Resize to " + width + "x" + height);
-	stage.setViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), false);
+	Vector2 size = Scaling.fit.apply(800, 480, width, height);
+        int viewportX = (int)(width - size.x) / 2;
+        int viewportY = (int)(height - size.y) / 2;
+        int viewportWidth = (int)size.x;
+        int viewportHeight = (int)size.y;
+        Gdx.gl.glViewport(viewportX, viewportY, viewportWidth, viewportHeight);
+        stage.setViewport(width, height, true, viewportX, viewportY, viewportWidth, viewportHeight);
     }
 
     @Override
